eafaf3b82460f839c2e1391479c78c7c4e5a67a9
sudhanjaswal-cmd <sudhanjaswal@gmail.com>
2026-05-20 16:33:34 +0530

    Handlers

--- src/app.js
+++ src/app.js
@@ -1,10 +1,6 @@
 const express = require("express");
 const app = express();
 
-app.use((req, res) => {
-  res.send("Sudhanshu jaswal");
-});
-
 app.listen(3000, () => {
   console.log("Server running successfully");
 });
